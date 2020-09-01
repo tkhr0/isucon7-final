e810230bf1e31bf51cce6c56a75ad9f9043ee53e
KarasawaTakahiro <karasawa@basicinc.jp>
2020-09-01 22:20:04 +0900

    fix deploy

--- webapp/ruby/deploy.sh
+++ webapp/ruby/deploy.sh
@@ -5,11 +5,11 @@ source ~/.profile
 ps aux | grep "puma 3" | grep -v grep | awk '{ print "kill -9", $2 }' | sh
 
 # git
-cd /home/ubuntu/work/isucon7-final
+cd /home/ubuntu/cco
 git checkout .
 git pull
 
 # start puma
-cd /home/ubuntu/work/isucon7-final/webapp/ruby
+cd /home/ubuntu/cco/webapp/ruby
 RACK_ENV=production bundle
 RACK_ENV=production bundle exec puma -C puma_config.rb -e production
