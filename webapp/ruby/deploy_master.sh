ddfcea1cc7c0b3f2a0600d24fdbbde539002cb9d
KarasawaTakahiro <karasawa@basicinc.jp>
2020-09-05 18:25:48 +0900

    change deploy shell

--- webapp/ruby/deploy_master.sh
+++ webapp/ruby/deploy_master.sh
@@ -1,9 +1,4 @@
-servers=(
-  18.183.182.198
-  54.168.57.16
-)
-
-for ip in "${servers[@]}" ; do
+for ip in "$@" ; do
     echo "[ ${ip} ]"
     ssh -i ~/Desktop/isucon7-final.pem ubuntu@${ip} 'bash /home/ubuntu/cco/webapp/ruby/deploy.sh'
 done
